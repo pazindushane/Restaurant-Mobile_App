f719e5416a3deee84df0c18514aeb1d0b2dd981c
pazindushane <shanepazindu7399@gmail.com>
2021-11-22 00:22:50 +0530

    Start Screen Creation Started

--- Restaurent2/Screens/MainScreens/StartScreen.js
+++ Restaurent2/Screens/MainScreens/StartScreen.js
@@ -1,18 +1,43 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import { Container, Content, NativeBaseProvider } from 'native-base';
 
 export default class StartScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
   render() {
     return (
-      <View>
-        <Text> StartScreen </Text>
+      // <NativeBaseProvider>
+      <View style={styles.bgclr}>
+        <View style={styles.bgclr2}></View>
+        <View style={styles.bgclr3}></View>
       </View>
-    );
+        /* <Content style={styles.bgclr2}></Content> */
+      // 
+      // </NativeBaseProvider>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  bgclr:{
+    flex:1,
+    backgroundColor : '#FFC013' 
+  },
+  bgclr2:{
+    flex:3,
+    flexDirection:'row',
+    alignItems:'center',
+    // backgroundColor: '#272C2F',
+    backgroundColor : '#FFC013',
+    width:360,
+    height:240,
+    // borderTopLeftRadius:5
+    
+  },
+  bgclr3:{
+    flex:2,
+    // backgroundColor : '#FFC013' 
+    backgroundColor: '#272C2F',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+})
